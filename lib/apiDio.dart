5c94b79d64b77a086181c60eaea66675ea1bc0ba
graceyaob <yaograceb@gmail.com>
2023-10-23 16:52:55 +0000

    stockage du token en local

--- lib/apiDio.dart
+++ lib/apiDio.dart
@@ -3,7 +3,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Dio dio = Dio();
 Future apipost() async {
-  var token;
+  String token;
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
   try {
     Response response = await dio.post(
         "https://dpi-backend-develop.winlogic.pro/users/v1/login/",
@@ -13,75 +15,45 @@ Future apipost() async {
 
       //Recupération du token
       token = response.data['token']['access'];
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('token', token);
 
-      return token;
+      //return token;
+      return prefs;
     } else {
       print("erreur: ${response.statusCode}");
     }
   } on DioException catch (e) {
     print("erreur : $e");
-    return token = null; // En cas d'erreur, retourne null
+    // En cas d'erreur, retourne null
   }
 }
 
-/*Future apiget() async {
+Future apiget() async {
+  print('This is message from dio');
+
+  //recupéré le token stocké en local
+  SharedPreferences token = await apipost();
+  final String? jeton = token.getString('token');
+  print('this is token $token');
+  dio.options.headers['Authorization'] = 'Bearer $jeton';
+  //if (token != null) {
   try {
-    
-    String token = await apipost();
-    if (token != null) {
-      dio.options.headers['Authorization'] = 'Bearer $token';
-      final CancelToken cancelToken = CancelToken();
-      await dio
-          .get("https://dpi-backend-develop.winlogic.pro/accueils/v1/modules/",
-              cancelToken: cancelToken)
-          .then((response) {
-        if (response.statusCode == 200) {
-          List data = response.data;
-          List first20 = data.take(20).toList();
-          return first20;
-        } else {
-          print("erreur statut = ${response.statusCode}");
-        }
-      }).catchError((error) {
-        if (CancelToken.isCancel(error)) {
-          // La requête a été annulée
-          print('Requête annulée');
-        } else {
-          // Gérer d'autres erreurs
-        }
-      });
+    Response response = await dio
+        .get("https://dpi-backend-develop.winlogic.pro/accueils/v1/modules/");
+    print('This is message from dio 2');
+
+    if (response.statusCode == 200) {
+      print("Requete GET envoyé avec succès");
+      List data = response.data;
+      List first20 = data.take(20).toList();
+      return first20;
+    } else {
+      print("erreur statut = ${response.statusCode}");
     }
   } catch (e) {
     print('erreur: $e');
   }
-  return []; //Retourne une liste vide en cas d'erreur
-}*/
-
-Future apiget() async {
-  print('This is message from dio');
-  var token = await apipost();
-  print('this is token $token');
-  dio.options.headers['Authorization'] = 'Bearer $token';
-  if (token != null) {
-    try {
-      Response response = await dio
-          .get("https://dpi-backend-develop.winlogic.pro/accueils/v1/modules/");
-      print('This is message from dio 2');
-
-      if (response.statusCode == 200) {
-        print("Requete GET envoyé avec succès");
-        List data = response.data;
-        List first20 = data.take(20).toList();
-        return first20;
-      } else {
-        print("erreur statut = ${response.statusCode}");
-      }
-    } catch (e) {
-      print('erreur: $e');
-    }
-  } else {
+  /*} else {
     print("désole");
-  }
+  }*/
 }
